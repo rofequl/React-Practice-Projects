e3cd148b5d655398c2bb0a5c299348fe45104a1c
Nayem <rofequlislamnayem@gmail.com>
2026-04-21 22:15:10 +0600

    Expense Tracker: Backend authentication API developed

--- Expense Tracker/backend/controllers/authController.js
+++ Expense Tracker/backend/controllers/authController.js
@@ -40,8 +40,36 @@ exports.registerUser = async (req, res) => {
 
 // Login User
 exports.loginUser = async (req, res) => {
+    const {email, password} = req.body;
+    if (!email || !password) {
+        return res.status(400).json({message: "All fields are required"})
+    }
+
+    try {
+        const user = await User.findOne({email});
+        if (!user || !(await user.comparePassword(password))) {
+            return res.stutus(400).json({message: "Invalid credentials"})
+        }
+
+        res.status(200).json({
+            id: user._id,
+            user,
+            token: generateToken(user._id)
+        })
+    } catch (error) {
+        res.status(500).json({message: "Error logging user", error: error.message})
+    }
 }
 
-// User Profile
+// Get User Info
 exports.getUserInfo = async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id).select("-password");
+
+        if (!user) return res.status(404).json({message: "No user with this id"})
+
+        res.status(200).json(user)
+    } catch (error) {
+        res.status(500).json({message: "Error logging user", error: error.message})
+    }
 }
